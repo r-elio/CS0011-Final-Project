ad300f450ea57dd63535c7f57c054344855a5673
Rajan Elio <r-elio@users.noreply.github.com>
2020-02-20 22:54:45 +0800

    remake project

--- app/src/main/java/android/application/meta/MainActivity.java
+++ app/src/main/java/android/application/meta/MainActivity.java
@@ -2,37 +2,13 @@ package android.application.meta;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toolbar;
-
-import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
-    TextInputEditText username;
-    TextInputEditText password;
-    Button login;
-    TextView register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        username = findViewById(R.id.username);
-        password = findViewById(R.id.password);
-        login = findViewById(R.id.login);
-        register = findViewById(R.id.register);
-
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,HomeActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 }
