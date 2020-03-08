8f887f57ea28e0d6f40d745fa162abd4331c3072
Rajan Elio <r-elio@users.noreply.github.com>
2020-03-08 16:17:33 +0800

    Update NotificationService.java

--- app/src/main/java/android/application/meta/NotificationService.java
+++ app/src/main/java/android/application/meta/NotificationService.java
@@ -7,7 +7,6 @@ import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Build;
-import android.os.Handler;
 import android.os.IBinder;
 
 import androidx.core.app.NotificationCompat;
